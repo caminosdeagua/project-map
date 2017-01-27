e217e40f574278a4db0c53b8d5ee101102dd7907
Dylan-Caminos <dylanterrell@Dylans-MacBook.local>
2017-01-27 16:31:33 -0600

    llml

--- data/project_map_dataset.js
+++ data/project_map_dataset.js
@@ -1,7 +1,7 @@
 var PROJECT_MAP_DATA =
 [
   {
-    "Community": "Agustín González",
+    "Community": "Agustin Gonzalez",
     "Latitude": 20.860267,
     "Longitude": -100.853317,
     "Project Name": "EWB-UCL Cistern Projects",
