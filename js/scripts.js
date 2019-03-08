bc21aa6b42ea1a7b2859b63d8b1e4c7cafd4deb4
ask53 <askrupp@gmail.com>
2019-03-07 19:09:33 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2086,6 +2086,6 @@ function adjustDisplayForMobile() {
 	document.getElementById("overlay_msg").style.fontSize = "22px";
 	var displays = document.getElementsByClassName("display_box");
 	for(var i=0; i<displays.length; i++) {
-		displays[i].style.width = "40%";		
+		displays[i].style.width = "85%";		
 	}
 }
